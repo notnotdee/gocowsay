882d609e1d17093a8fe244d0ad71782799799e3c
dl-watson <dlarkinwatson@gmail.com>
2021-06-02 15:25:33 -0700

    comments

--- main.go
+++ main.go
@@ -76,34 +76,23 @@ func buildBubble(lines []string, width int) string {
 }
 
 func main() {
-	// os.Stdin.Stat() returns an os.FileInfo
 	info, err := os.Stdin.Stat()
 	if err != nil {
-		panic(err)
-	}
-	// os.FileInfo.Mode() returns a FileMode flag, which represents a file's mode and permission bits.
-	// The .Mode() looks like this: `$ Dcrw--w----`
-	// The flag we are looking for is os.ModeNamedPipe. When this flag is on it means that we have a pipe.
-	// This way we can know when our command is receiving stdout from another process.
-	if info.Mode()&os.ModeNamedPipe == 0 {
-		fmt.Println("This command is intended to work with pipes.")
-		// fortune is a CLI tool that prints a random quotation.
-		// fmt.Println("Usage: fortune | ./gocowsay")
+		fmt.Println(err)
 		return
 	}
 
-	// At this point, you can query `$ fortune | ./gocowsay` in your CLI
+	if info.Mode()&os.ModeNamedPipe == 0 {
+		fmt.Println("This command is intended to work with pipes.")
+		return
+	}
 
 	var lines []string
-	// The bufio package provides a buffered I/O in GoLang.
-	// NewReader returns a new Reader whose buffer has the default size.
-	// We're passing the NewReader the result of the stdin.
 	reader := bufio.NewReader(os.Stdin)
 
-	// ReadRune reads a single UTF-8 encoded Unicode character and returns the rune and its size in bytes. If the encoded rune is invalid, it consumes one byte and returns unicode.ReplacementChar (U+FFFD) with a size of 1.
-	// EOF is the error returned by Read when no more input is available.
 	for {
 		line, _, err := reader.ReadLine()
+		reader.Buffered()
 		if err != nil && err == io.EOF {
 			break
 		}
